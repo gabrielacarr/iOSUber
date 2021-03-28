2627fbe969e4e742859a7861fc0a4fdedf39efd8
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-03-28 11:34:04 -0400

    Displays: HomeMap + HomeSearch + Covid Message, styles integrated, renders correctly, looking good

--- Uber/src/components/HomeSearch/index.js
+++ Uber/src/components/HomeSearch/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import styles from './styles';
 
 const HomeSearch = () => {
   return (
     <View>
-      <Text>HomeSearch component</Text>
+      {/* Input Box */}
+      <View style={styles.inputBox}>
+        <Text style={styles.inputText}>Where to? </Text>
+
+        <View style={styles.timeContainer}>
+          <AntDesign name={'clockcircle'} size={15} color={'#707070'} />
+          <Text>Now</Text>
+          <MaterialIcons name={'keyboard-arrow-down'} size={15} />
+        </View>
+      </View>
+      {/* Previous Destination */}
+
+      {/* Home Destination */}
     </View>
   );
 };
---
+++ Uber/src/components/HomeSearch/styles.js
@@ -0,0 +1,26 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  inputBox: {
+    backgroundColor: '#d9d9d9',
+    margin: 10,
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  inputText: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#6e6e6e',
+  },
+  timeContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: 100,
+    padding: 10,
+    backgroundColor: '#fff',
+    borderRadius: 50,
+  },
+});
+export default styles;
