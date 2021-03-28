e61bcfdbf6954f4597962029a81bd344b901f88d
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-03-28 12:09:16 -0400

    Displays: Destination Search renders correctly, on a roll here

--- Uber/App.js
+++ Uber/App.js
@@ -10,14 +10,16 @@ import React from 'react';
 
 import {SafeAreaView, StatusBar} from 'react-native';
 
-import HomeScreen from './src/screens/HomeScreen';
+// import HomeScreen from './src/screens/HomeScreen';
+import DestinationSearch from './src/screens/DestinationSearch';
 
 const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <HomeScreen />
+        {/* <HomeScreen /> */}
+        <DestinationSearch />
       </SafeAreaView>
     </>
   );
