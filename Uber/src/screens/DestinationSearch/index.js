7e9e93b7d5fc2de7a9bcc29744c2cade50ab1333
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-03-28 12:27:15 -0400

    Displays: Destination Search, implemented useState + styles, renders correctly

--- Uber/src/screens/DestinationSearch/index.js
+++ Uber/src/screens/DestinationSearch/index.js
@@ -1,11 +1,34 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, TextInput, SafeAreaView} from 'react-native';
+import styles from './styles';
+/***
+ * TODO:
+ * Location input boxes
+ * default locations(home, work)
+ * location row
+ ***/
 
 const DestinationSearch = () => {
+  const [fromText, setFromText] = useState('');
+  const [destinationText, setDestinationText] = useState('');
   return (
-    <View>
-      <Text>DestinationSearch component on the screen</Text>
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <TextInput
+          value={fromText}
+          onChangeText={setFromText}
+          style={styles.textInput}
+          placeholder="From"
+        />
+
+        <TextInput
+          value={destinationText}
+          onTextChange={setDestinationText}
+          style={styles.textInput}
+          placeholder="Where to?"
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
