1fb20f0c101e0c527ab4abf25b24342cb65fa9c3
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-03-28 18:59:26 -0400

    Displays: Type Row styled and ready to render multiple

--- Uber/src/components/UberTypeRow/index.js
+++ Uber/src/components/UberTypeRow/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Image, Text} from 'react-native';
 import styles from './styles';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const UberTypeRow = () => {
   return (
-    <View>
-      <Text>Uber Type Rows components in the view</Text>
+    <View style={styles.container}>
+      {/* Image */}
+      <Image
+        style={styles.image}
+        source={require('../../assets/images/UberX.jpeg')}
+      />
+
+      <View style={styles.middleContainer}>
+        <Text style={styles.type}>
+          UberX {''}
+          <Ionicons name={'person'} size={15} />3
+        </Text>
+
+        <Text style={styles.time}>8:30 PM drop off</Text>
+      </View>
+
+      <View style={styles.rightContainer}>
+        <Ionicons name={'pricetag'} size={12} color={'#42d742'} />
+
+        <Text style={styles.price}>est. $27</Text>
+      </View>
     </View>
   );
 };
--- Uber/src/screens/SearchResults/index.js
+++ Uber/src/screens/SearchResults/index.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {View, Text} from 'react-native';
 import HomeMap from '../../components/HomeMap';
+import UberTypeRow from '../../components/UberTypeRow';
 import UberTypes from '../../components/UberTypes';
 
 const SearchResults = () => {
