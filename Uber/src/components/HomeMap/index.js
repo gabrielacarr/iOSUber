c4eb013db74e23df58b3d2f152844da7499eede5
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-03-28 00:43:13 -0400

    Displays: directory architecture established, renders correctly

---
+++ Uber/src/components/HomeMap/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const HomeMap = () => {
+  return (
+    <View
+      style={{
+        height: 300,
+        backgroundColor: '#a0abff',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>I am you Home Map component </Text>
+    </View>
+  );
+};
+export default HomeMap;
---
+++ Uber/src/screens/HomeScreen/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import HomeMap from '../../components/HomeMap';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <HomeMap />
+
+      {/* Covid Message */}
+      {/* Bottom Component */}
+    </View>
+  );
+};
+export default HomeScreen;
