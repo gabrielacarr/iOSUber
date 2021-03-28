27c066496fde45b77345647f80f3425940bfcfbb
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-03-28 17:52:46 -0400

    Displayed: Type Row component, renders correcly

---
+++ Uber/src/components/UberTypeRow/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from './styles';
+
+const UberTypeRow = () => {
+  return (
+    <View>
+      <Text>Uber Type Rows components in the view</Text>
+    </View>
+  );
+};
+export default UberTypeRow;
--- Uber/src/components/UberTypes/index.js
+++ Uber/src/components/UberTypes/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import styles from './styles';
+import UberTypeRow from '../UberTypeRow';
 
 const UberTypes = () => {
   return (
     <View>
       <Text>Uber Type components in the view</Text>
+      <UberTypeRow />
     </View>
   );
 };
