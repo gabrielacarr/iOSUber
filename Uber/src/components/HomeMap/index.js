1266642e84eeacb1d30521209362237bcc205bf5
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-03-28 21:27:26 -0400

    Displays: the car, adjusted the dimensions of the maps, looking good

--- Uber/src/components/HomeMap/index.js
+++ Uber/src/components/HomeMap/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {View, Image, Dimensions} from 'react-native';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 
 const HomeMap = () => {
   return (
     <View
       style={{
-        height: 400,
+        height: Dimensions.get('window').height - 390,
         backgroundColor: '#a0abff',
         justifyContent: 'center',
         alignItems: 'center',
@@ -19,8 +19,14 @@ const HomeMap = () => {
           longitude: -122.4324,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
-        }}
-      />
+        }}>
+        <Marker coordinate={{latitude: 37.78825, longitude: -122.4324}}>
+          <Image
+            style={{width: 70, height: 70, resizeMode: 'contain'}}
+            source={require('../../assets/images/top-UberX.png')}
+          />
+        </Marker>
+      </MapView>
     </View>
   );
 };
