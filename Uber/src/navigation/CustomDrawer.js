1fb698f65b8dc35dbc9c98b0ebce51222ec18a88
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-03-30 00:04:27 -0400

    Completed all tasks for today: side bar style updated

--- Uber/src/navigation/CustomDrawer.js
+++ Uber/src/navigation/CustomDrawer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {View, Text, Pressable} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Entypo from 'react-native-vector-icons/Entypo';
 import {
   DrawerContentScrollView,
   DrawerItemList,
@@ -10,17 +12,68 @@ const CustomDrawer = props => {
     <DrawerContentScrollView {...props}>
       <View style={{backgroundColor: 'black', padding: 15}}>
         {/* User Row */}
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            paddingVertical: 25,
+          }}>
+          <View
+            style={{
+              backgroundColor: '#cacaca',
+              width: 50,
+              height: 50,
+              borderRadius: 25,
+              marginRight: 10,
+            }}>
+            <Entypo name={'user'} size={49} color={'black'} />
+          </View>
+          <View>
+            <Text style={{color: 'white', fontSize: 20}}>Gabriela Cardo</Text>
+            <Text style={{color: 'lightgrey'}}>
+              5.00 <AntDesign name={'star'} size={13} color={'#DFDEDE'} />
+            </Text>
+          </View>
+        </View>
 
         {/* Messages Row */}
-
+        <View
+          style={{
+            borderTopColor: '#3A3A3A',
+            borderTopWidth: 1,
+            borderBottomWidth: 1,
+            borderBottomColor: '#3A3A3A',
+            paddingVertical: 25,
+            marginVertical: 10,
+          }}>
+          <Pressable
+            onPress={() => {
+              console.warn('Messages');
+            }}>
+            <Text style={{color: '#DFDEDE', flexDirection: 'row'}}>
+              Messages
+              <AntDesign name={'right'} size={13} color={'#DFDEDE'} />
+            </Text>
+          </Pressable>
+        </View>
         {/* Do More row */}
+        <Pressable
+          onPress={() => {
+            console.warn('Do more');
+          }}>
+          <Text style={{color: '#545353', paddingVertical: 5}}>
+            Do more with your account
+          </Text>
+        </Pressable>
 
         {/* Make Money Row */}
         <Pressable
           onPress={() => {
             console.warn('Make money');
           }}>
-          <Text style={{color: 'white'}}>Make me money driving</Text>
+          <Text style={{color: '#DFDEDE', paddingVertical: 5}}>
+            Make me money driving
+          </Text>
         </Pressable>
       </View>
       <DrawerItemList {...props} />
