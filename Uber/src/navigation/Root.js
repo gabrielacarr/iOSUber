68584147aca35fcf6f7d50e40f7d6c34fee94ea5
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-03-29 22:27:48 -0400

    Integration: Dummy data for drawer navigation

--- Uber/src/navigation/Root.js
+++ Uber/src/navigation/Root.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createDrawerNavigator} from '@react-navigation/drawer';
@@ -6,12 +7,33 @@ import HomeNavigation from './Home';
 
 const Drawer = createDrawerNavigator();
 
+const DummyScreen = props => (
+  <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text>{props.name}</Text>
+  </View>
+);
+
 const RootNavigation = () => {
   return (
     <NavigationContainer>
-      {/* <HomeNavigation /> */}
       <Drawer.Navigator>
         <Drawer.Screen name="Home" component={HomeNavigation} />
+
+        <Drawer.Screen name="Your Trips">
+          {() => <DummyScreen name={'Your Trips'} />}
+        </Drawer.Screen>
+
+        <Drawer.Screen name="Help">
+          {() => <DummyScreen name={'Help'} />}
+        </Drawer.Screen>
+
+        <Drawer.Screen name="Wallet">
+          {() => <DummyScreen name={'Wallet'} />}
+        </Drawer.Screen>
+
+        <Drawer.Screen name="Settings">
+          {() => <DummyScreen name={'Settings'} />}
+        </Drawer.Screen>
       </Drawer.Navigator>
     </NavigationContainer>
   );
